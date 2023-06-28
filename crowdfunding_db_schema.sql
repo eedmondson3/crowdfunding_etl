8e6ba560b00415b14913738914027b54909745ae
eedmondson3 <edmondsonedwin@gmail.com>
2023-06-27 19:24:47 -0700

    Finished Challenge

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,70 @@
+DROP table "Campaign"
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(60)   NOT NULL,
+    "description" varchar(120)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(30)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(30)   NOT NULL,
+    "currency" varchar(30)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(30)   NOT NULL,
+    "subcategory_id" varchar(30)   NOT NULL
+);
+
+CREATE TABLE "Category" (
+    "category_id" varchar(30)   NOT NULL,
+    "category" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" varchar(30)   NOT NULL,
+    "subcategory" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "email" varchar(60)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
+-- Verify data upload and table creation
+select * 
+from "Campaign"
+limit 5
+
+select * 
+from "Category"
+limit 5
+
+select * 
+from "Contacts"
+limit 5
+
+select * 
+from "Subcategory"
+limit 5
